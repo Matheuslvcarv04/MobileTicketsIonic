ba4a9c7afd0cc24cbdb96f973f69d8155158c583
Lucas Alves <Lucasfael04@gmail.com>
2026-03-29 19:45:06 -0300

    Correção final de rotas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 import { TicketService } from '../services/ticket.service';
 
 @Component({
   selector: 'app-tab1',
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   senhaGerada: any;
 
   constructor(private ticketService: TicketService) {}
@@ -15,5 +19,4 @@ export class Tab1Page {
   gerar(tipo: 'SP' | 'SG' | 'SE') {
     this.senhaGerada = this.ticketService.gerarSenha(tipo);
   }
-
 }
